6fbc064b7378e1be2267174546555071e323e238
sowmya2503 <59363553+sowmya2503@users.noreply.github.com>
2026-01-29 20:18:07 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (11.95%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,14 +1,9 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        """
-        Finds the index of the first occurrence of needle in haystack.
+        k=-1
+        try:
+            k = haystack.index(needle)
+        except:
+            pass
+        return k
         
-        Args:
-            haystack: The string to search within.
-            needle: The substring to search for.
-            
-        Returns:
-            The index of the first occurrence of needle in haystack, 
-            or -1 if needle is not part of haystack.
-        """
-        return haystack.find(needle)
