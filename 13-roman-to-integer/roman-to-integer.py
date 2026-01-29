4949411ec0489352f41df9e7db8352715fa5eae2
sowmya2503 <59363553+sowmya2503@users.noreply.github.com>
2026-01-29 20:45:23 +0530

    Time: 11 ms (15.61%) | Memory: 19.4 MB (24.93%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,21 +1,18 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        roman_map = {
-            'I': 1, 'V': 5, 'X': 10, 'L': 50,
-            'C': 100, 'D': 500, 'M': 1000
-        }
-        
-        total = 0
-        n = len(s)
-        
-        for i in range(n):
-            current_value = roman_map[s[i]]
-            
-            # Check for the subtractive case
-            if i < n - 1 and current_value < roman_map[s[i+1]]:
-                total -= current_value
+        d={'I':             1,
+'V'             :5,
+'X'            : 10,
+'L'            : 50,
+'C'             :100,
+'D'         : 500,
+'M'             :1000}
+        count=0
+        for i in range(len(s)):
+            if i+1<len(s) and d[s[i]]<d[s[i+1]]:
+                count-=d[s[i]]
             else:
-                total += current_value
-                
-        return total
+                count+=d[s[i]]
+        return count
+        
         
