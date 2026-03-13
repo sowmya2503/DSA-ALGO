9e2e1fe48e50cf70390fca31e858872bc7acc2e5
sowmya2503 <59363553+sowmya2503@users.noreply.github.com>
2026-03-13 21:35:07 +0530

    Time: 7 ms (81.40%) | Memory: 19.5 MB (98.53%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,22 +1,16 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        left = 0
-        right = len(s) - 1
-
-        while left < right:
-            # Move left pointer until an alphanumeric character is found
-            while left < right and not s[left].isalnum():
-                left += 1
-            # Move right pointer until an alphanumeric character is found
-            while left < right and not s[right].isalnum():
-                right -= 1
-
-            # Compare characters (case-insensitive)
-            if s[left].lower() != s[right].lower():
-                return False
-            
-            # Move pointers inward
-            left += 1
-            right -= 1
+        l=0
+        n=""
         
+        for ch in s:
+            if ch.isalnum():
+                n+=ch.lower()
+        r=len(n)-1
+        while l<r:
+            if n[l]!=n[r]:
+                return False
+            l+=1
+            r-=1
         return True
+        
