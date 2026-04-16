bde52fa5b585f5319ee93b6a31820be1fd5647ee
sowmya2503 <59363553+sowmya2503@users.noreply.github.com>
2026-04-16 16:21:42 +0530

    Time: 11 ms (25.56%) | Memory: 22.3 MB (98.01%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -6,8 +6,8 @@ class Solution:
         for i in range(1,len(intervals)):
             prev=res[-1]
             curr=intervals[i]
-            if curr[0]<=prev[1]:
-                prev[1]=max(curr[1],prev[1])
+            if curr[0]<= prev[1]:
+                prev[1] = max(prev[1],curr[1])
             else:
                 res.append(curr)
         return res
