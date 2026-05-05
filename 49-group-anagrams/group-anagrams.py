b7f3d38b23fb49c08d96864d0e1d30bc959a9185
sowmya2503 <59363553+sowmya2503@users.noreply.github.com>
2026-05-05 11:44:55 +0530

    Time: 11 ms (84.44%) | Memory: 22 MB (67.70%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,12 +1,10 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         from collections import defaultdict
-        l=[]
         d=defaultdict(list)
         for word in strs:
             k=''.join(sorted(word))
             d[k].append(word)
-        for k,v in d.items():
-            l.append(v)
-        return l
+        return list(d.values())
+            
         
